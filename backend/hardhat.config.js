46d5052df99bc6be7495ac03b5a2b65c4664d347
sushiparlour <89986845+PxGnome@users.noreply.github.com>
2023-04-14 22:19:14 +0900

    Added hardhat config with basic contract and test

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   solidity: {
-    version: "0.8.9",
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
@@ -11,18 +11,52 @@ module.exports = {
     },
   },
   allowUnlimitedContractSize: true,
+  defaultNetwork: "hardhat",
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    excludeContracts: [],
+    src: "./contracts",
+    coinmarketcap: process.env.CMC_API,
+  },
   networks: {
-    hardhat: {},
+    hardhat: {
+      accounts: {
+        count: 10,
+        mnemonic: process.env.MNEMONIC,
+        initialIndex: Number(process.env.INITIAL_INDEX),
+      },
+      chainId: 31337,
+      gas: "auto",
+      blockGasLimit: 1000000000000
+    },
     POLYGON_ZKEVM_MAINNET: {
-      accounts: [`${process.env.PRIVATE_KEY}`],
+      accounts: {
+        count: 10,
+        mnemonic: process.env.MNEMONIC,
+        initialIndex: Number(process.env.INITIAL_INDEX),
+      },
+      chainId: 1101,
       url: `https://polygonzkevm-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
     },
     POLYGON_ZKEVM_TESTNET: {
-      accounts: [`${process.env.PRIVATE_KEY}`],
+      accounts: {
+        count: 10,
+        mnemonic: process.env.MNEMONIC,
+        initialIndex: Number(process.env.INITIAL_INDEX),
+      },
+      chainId: 1442,
       url: `https://polygonzkevm-testnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
     },
   },
   etherscan: {
     apiKey: `${process.env.ETHERSCAN_API_KEY}`,
   },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  }
 };
+
